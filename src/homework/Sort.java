3bfdd1548afc738b7ce1dcfba6a77e25aaf51ed6
Hrach <hrach.margaryan.9595@mail.ru>
2025-10-07 12:28:09 +0400

    homework.4.

---
+++ src/homework/Sort.java
@@ -0,0 +1,23 @@
+package homework;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] array = {4,7,1,3,9,0,2};
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        for(int num : array) {
+            System.out.print(num + " ");
+        }
+
+
+    }
+}
