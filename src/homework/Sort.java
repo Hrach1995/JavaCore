57185de017c5828c82177dcd1876aa33a4ca6510
Hrach <hrach.margaryan.9595@mail.ru>
2025-10-07 15:26:44 +0400

    homework.4

--- src/homework/Sort.java
+++ src/homework/Sort.java
@@ -5,11 +5,11 @@ public class Sort {
         int[] array = {4,7,1,3,9,0,2};
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
                 }
             }
         }
