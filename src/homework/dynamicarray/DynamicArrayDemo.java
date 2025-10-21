e77754ec15ed17377441ba8d22e41b0972f539f9
Hrach <hrach.margaryan.9595@mail.ru>
2025-10-21 18:32:13 +0400

    HomeWork.DynamicArray2

--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -5,25 +5,34 @@ public class DynamicArrayDemo {
 
         DynamicArray da = new DynamicArray();
 
-        da.add(1);
-        da.add(2);
-        da.add(3);
-        da.add(4);
-        da.add(5);
-        da.add(6);
-        da.add(7);
-        da.add(8);
-        da.add(9);
-        da.add(10);
-        da.add(11);
-        da.add(12);
-
+        for (int i = 0; i <= 10; i++) {
+            da.add(i);
+        }
         da.print();
+
+        da.set(2, 999);
+        da.print();
+
+        System.out.println("---------------");
+
+        da.add(4, 555);
+        da.print();
+
+        System.out.println("-----------------");
+
+        da.deleteByindex(2);
+        da.print();
+
+        System.out.println("---------------------------");
+
+        System.out.println("exists(999) → " + da.exists(999));
+        System.out.println("exists(8) → " + da.exists(8));
 
         System.out.println("------------");
         System.out.println(da.getByIndex(0));
         System.out.println(da.getByIndex(5));
         System.out.println(da.getByIndex(13));
+
 
 
     }
